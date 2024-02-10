0bfffed20ab0b76dbb8c9ad764b94586c5a63fc8
vda1r22 <vda1r22@soton.ac.uk>
2024-02-10 21:40:56 +0000

    Updates 10 Feb

---
+++ bbnet/R/Priors.R
@@ -0,0 +1,14 @@
+#' Priors data
+#'
+#' A subset of data from the published work:
+#' Can Coral Reef Restoration Programmes Facilitate Changes in Environmental Attitudes?
+#' A Case Study on a Rural Fisher Community in North Bali, Indonesia
+#' Boakes et al. 2023
+#'
+#' @format
+#' A data frame with 19 rows and 2 columns:
+#' \describe{
+#'   \item{increase}{Integer}
+#'   \item{X}{Variable names}
+#' }
+#' @source <https://doi.org/10.1007/s10745-023-00452-7>
---
+++ bbnet/test/Test.R
@@ -0,0 +1,19 @@
+# Testing function
+
+data("BBNfile", "priors1", "priors2", "priors3") #Load data examples
+
+input.files(BBNfile, priors1, priors2, priors3) # set up of all the BBN models
+
+# bbn.predict(boot_max, values, figure)
+# boot_max is the number of bootstraps to perform - default of 1
+# values -  1 (default) prints the final values and confidence intervals of each scenario, 0 (or any other value) excludes these as an output
+# figure - 0 no figures are produced, 1 (default) PDF is saved to working directory, 2 figure is presented in figure panel
+# can be run simply as BBN.predict()
+
+bbn.predict()
+
+bbn.predict(boot_max = 1000, values =0, figure = 2)
+
+# Things to maybe include:
+# figure option 3, prints figures independently, not grouped as all scenarios together
+# some options for the figures, such as font sizes
